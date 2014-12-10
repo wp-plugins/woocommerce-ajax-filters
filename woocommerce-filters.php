2e79524ae60392ab30ec90bf02183475776c0290
dholovnia <dholovnia@b8457f37-d9ea-0310-8a92-e5e31aec5664>
2014-12-10 13:16:05 +0000

    initial commit 1.0.1
    git-svn-id: https://plugins.svn.wordpress.org/woocommerce-ajax-filters/trunk@1041900 b8457f37-d9ea-0310-8a92-e5e31aec5664

---
+++ woocommerce-filters.php
@@ -0,0 +1,19 @@
+<?php
+/*
+	Plugin Name: Advanced AJAX Product Filters for WooCommerce
+	Plugin URI: http://berocket.com/wp-plugins/product-filters
+	Description: Advanced AJAX Product Filters for WooCommerce
+	Version: 1.0.1
+	Author: BeRocket
+	Author URI: http://berocket.com
+*/
+
+require_once dirname( __FILE__ ).'/includes/widget.php';
+
+/**
+ * Class BeRocket_AAPF
+ * will be added on next release. There are no global options for now so no need in this class
+ */
+
+class BeRocket_AAPF {}
+new BeRocket_AAPF;
