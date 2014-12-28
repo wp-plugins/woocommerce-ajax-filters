85aeabb3a3dde435d5bc004e02115cd40f46d89e
dholovnia <dholovnia@b8457f37-d9ea-0310-8a92-e5e31aec5664>
2014-12-28 18:30:59 +0000

    new version, with poker
    git-svn-id: https://plugins.svn.wordpress.org/woocommerce-ajax-filters/tags/1.0.3.3@1055702 b8457f37-d9ea-0310-8a92-e5e31aec5664

--- woocommerce-filters.php
+++ woocommerce-filters.php
@@ -3,7 +3,7 @@
 	Plugin Name: Advanced AJAX Product Filters for WooCommerce
 	Plugin URI: http://berocket.com/wp-plugins/product-filters
 	Description: Advanced AJAX Product Filters for WooCommerce
-	Version: 1.0.3.2
+	Version: 1.0.3.3
 	Author: BeRocket
 	Author URI: http://berocket.com
 */
